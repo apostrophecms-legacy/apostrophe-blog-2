6f3193f7c51a79717b682c24a7911564b503c9da
Tom Boutell <tom@punkave.com>
2014-11-13 10:38:31 -0500

    datestamp removal task must guarantee unique slugs

--- removeDateFrom.js
+++ removeDateFrom.js
@@ -4,13 +4,32 @@ module.exports = function(self, argv, callback) {
   return self._apos.forEachPage({ type: self.pieceName }, {}, function(piece, callback) {
     var oldSlug = piece.slug;
     self.pieces.addDateToSlug(piece, true);
-    return self._apos.pages.update({
-      slug: oldSlug
-    }, {
-      $set: {
-        slug: piece.slug
-      }
-    }, callback);
+
+    function attempt() {
+      return self._apos.pages.update({
+        slug: oldSlug
+      }, {
+        $set: {
+          slug: piece.slug
+        }
+      }, function(err) {
+        if (!err) {
+          return callback(null);
+        }
+        // Careful, removing the date can make
+        // slugs no longer unique, resolve that
+        if (self._apos.isUniqueError(err))
+        {
+          var num = (Math.floor(Math.random() * 10)).toString();
+          piece.slug += num;
+          return attempt();
+        }
+        return callback(err);
+      });
+    }
+
+    attempt();
+
   }, callback);
 };
 
